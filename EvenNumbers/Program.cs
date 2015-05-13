c1e8fb557e05af3242b4c946d7690dc546466d25
zahidulAlam <zahid.plabon@gmail.com>
2015-05-13 17:30:43 +0600

    1st Ver.

---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The EVEN Numbers between 0 to 100 are : ");
+            for(int i=0; i<=100; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
